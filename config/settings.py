09c4155267d4a10aeb2c4be0fb30caee4a7750ed
Nazim <nazimplabon@gmail.com>
2026-08-16 01:47:43 +0600

    Configure WhiteNoise for static files

--- config/settings.py
+++ config/settings.py
@@ -49,6 +49,7 @@ LOGIN_URL = '/dashboard/login/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,6 +127,10 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
